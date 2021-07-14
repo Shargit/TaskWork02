79007b7f0b5afd5595e17953a599666fd16d5945
Shar <peplok@mail.ru>
2021-07-14 19:20:05 +0400

    Второе задание

---
+++ src/TaskWork02.java
@@ -0,0 +1,8 @@
+public class TaskWork02 {
+    public static void main(String[] args) {
+        byte a = 12;
+        double b = 16.240;
+        System.out.println("byte a=" + a);
+        System.out.println("double b=" + b);
+    }
+}
